972c40e86f56a9739bd2b9d6d88507980624d41e
AracelyQuinta <am.quintanillar@uea.edu.ec>
2025-06-19 17:32:42 -0500

    creacion de una clase con el nombre del objeto habitación definiendo las caracteristicas basicas de la habitación(atributos)

--- semana 4/EjemplosMundoReal_POO/Ejemplos del Mundo Real y Características de la Programación Orientada a Objetos.py
+++ semana 4/EjemplosMundoReal_POO/Ejemplos del Mundo Real y Características de la Programación Orientada a Objetos.py
@@ -1,6 +1,7 @@
-class Habitacion:
+class Habitación:
     def __init__(self, numero, tipo, precio_por_hora, disponible=True):
         self.numero = numero
         self.tipo = tipo
         self.precio_por_hora = precio_por_hora
         self.disponible = disponible
+
