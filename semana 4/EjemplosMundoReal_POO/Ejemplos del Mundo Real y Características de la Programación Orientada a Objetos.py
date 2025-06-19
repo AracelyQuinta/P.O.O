a8c632298da1966232def13f413b4e509c98f4a7
AracelyQuinta <am.quintanillar@uea.edu.ec>
2025-06-19 14:25:19 -0500

    Ejemplos del mundo real

---
+++ semana 4/EjemplosMundoReal_POO/Ejemplos del Mundo Real y Características de la Programación Orientada a Objetos.py
@@ -0,0 +1,11 @@
+# Programación Orientada a Objetos (POO)
+# Ejemplo: Sistemas de Reservas de hoteles
+
+
+class Hoteleria:
+    def __init__(self, hotel,numero_de_habitacion, tipo_de_habitacion,disponibilidad,precio_de_la_habitacion):
+        self.numero_de_habitacion = numero_de_habitacion
+        self.tipo_de_habitacion = tipo_de_habitacion
+        self.disponibilidad = disponibilidad
+        self.precio_de_la_habitacion = precio_de_la_habitacion
+
