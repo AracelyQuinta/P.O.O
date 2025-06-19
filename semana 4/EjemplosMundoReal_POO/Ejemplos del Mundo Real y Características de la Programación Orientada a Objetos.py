be5d75c5d0738833b17704a48e335ceb28eff5c9
AracelyQuinta <am.quintanillar@uea.edu.ec>
2025-06-19 17:37:11 -0500

    creacion del nuevo directorio de EjemplosMundoReal_POO

--- semana 4/EjemplosMundoReal_POO/Ejemplos del Mundo Real y Características de la Programación Orientada a Objetos.py
+++
@@ -1,7 +0,0 @@
-class Habitacion:
-    def __init__(self, numero, tipo, precio_por_hora, disponible=True):
-        self.numero = numero
-        self.tipo = tipo
-        self.precio_por_hora = precio_por_hora
-        self.disponible = disponible
-
